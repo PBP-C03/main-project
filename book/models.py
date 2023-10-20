557e7b1efe3af5ea77be7b786efb3a666a45a1fa
Yosef <yosefnuragaw@gmail.com>
2023-10-20 17:28:52 +0700

    create database

---
+++ book/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    isbn = models.CharField(max_length=10)
+    title = models.TextField(blank=True, null=True)
+    author = models.TextField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
+    publisher = models.TextField(blank=True, null=True)
+    image = models.TextField(blank=True, null=True)
+    price = models.IntegerField(blank=True, null=True)
