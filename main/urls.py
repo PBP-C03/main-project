83f074fe44d498274f8ea2ffb84c5069f6ffc82a
afsarakhaa <afsarrakha140604@gmail.com>
2023-10-25 17:16:39 +0700

    Draft Main Page

---
+++ main/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from main.views import show_main
+from main.views import signup
+from main.views import login_user
+from main.views import logout_user
+
+app_name = 'main'
+
+urlpatterns = [
+    path('', show_main, name='show_main'),
+    path('signup/', signup, name='signup'),
+    path('login/', login_user, name='login'), 
+    path('logout/', logout_user, name='logout'),
+
+]
