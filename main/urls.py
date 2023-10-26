90ff6a63b1aa03c435ea0a0807a69223318e3f8f
Yosef <yosefnuragaw@gmail.com>
2023-10-26 11:55:26 +0700

    Profile page and navbar to base html

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main.views import show_main
 from main.views import signup
 from main.views import login_user
-from main.views import logout_user
+from main.views import logout_user,account_user,insert_balance,get_saldo
 
 app_name = 'main'
 
@@ -11,5 +11,7 @@ urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login_user, name='login'), 
     path('logout/', logout_user, name='logout'),
-
+    path('account/', account_user, name='account'),
+    path('insert_balance/', insert_balance, name='insert_balance'),
+    path('get_saldo/', get_saldo, name='get_saldo'),
 ]
