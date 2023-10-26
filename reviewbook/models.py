f50be12cd9cb9c04ef5ed1b12f0b034548b17397
steven-fo <stevenfo0711@gmail.com>
2023-10-26 13:22:43 +0700

    feat: add models and forms

--- reviewbook/models.py
+++ reviewbook/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+from book.models import Book
 
-# Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    date_added = models.DateField(auto_now_add=True)
+    rating = models.IntegerField()
+    review = models.TextField()
