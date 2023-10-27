e26b128a211a5c0afcf5b6504bccef40d2a1676c
afsarakhaa <afsarrakha140604@gmail.com>
2023-10-27 09:19:20 +0700

    Add Fitur CartBook V1

---
+++ cartbook/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from book.models import Book
+from django.contrib.auth.models import User
+
+# Create your models here.
+class Cart (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    total_amount = models.IntegerField(default=0)
+    total_harga = models.IntegerField(default=0)
+
+# # Buat Objek baru dalam bentuk Book
+class Book_Cart (models.Model): # Untuk Setiap Buku
+    amount = models.IntegerField(default=1)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE) # Banyak Buku bisa diletakkan di 1 CART User
+    carts = models.ForeignKey(Cart, on_delete=models.CASCADE) # Setiap Buku bisa dimiliki oleh banyak CART
