ad6f0cfdb5e668feff01e08c80e94bf04836e316
steven-fo <stevenfo0711@gmail.com>
2023-10-28 22:03:42 +0700

    feat: add addReview and deleteReview features

--- reviewbook/urls.py
+++ reviewbook/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from reviewbook.views import show_main, add_review_ajax, delete_review_ajax, get_reviews_ajax
+from reviewbook.views import show_review, add_review, delete_review, get_reviews, get_user_review, edit_review
 
 app_name = 'reviewbook'
 
 urlpatterns = [
-    path('<int:id>/', show_main, name="show_main"),
-    path('add-reqview/', add_review_ajax, name="add_review_ajax"),
-    path('delete-review/<int:id>/', delete_review_ajax, name="delete_review_ajax"),
-    path('get-reviews/<int:id>/', get_reviews_ajax, name="get_reviews_ajax")
+    path('<int:id>/', show_review, name="show_review"),
+    path('<int:id>/add-review/', add_review, name="add_review"),
+    path('<int:id>/delete-review/<int:reviewId>/', delete_review, name="delete_review"),
+    path('<int:id>/get-reviews/', get_reviews, name="get_reviews"),
+    path('<int:id>/get-user-review/', get_user_review, name="get_user_review"),
+    path('<int:id>/edit-review/<int:reviewId>/', edit_review, name="edit_review")
 ]
--- reviewbook/views.py
+++ reviewbook/views.py
@@ -1,46 +1,71 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from book.models import Book
 from reviewbook.models import Review
 from reviewbook.forms import ReviewForm
-from django.http import HttpResponseNotFound, HttpResponse
+from django.http import HttpResponseNotFound, HttpResponse, HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.decorators import login_required
 from django.core import serializers
 
 @login_required(login_url='/login')
-def show_main(request, id):
+def show_review(request, id):
     book = Book.objects.get(pk = id)
     reviews = Review.objects.filter(book = book)
-    review = Review.objects.filter(user=request.user, book=book)
+    totalRating = 0
+    for r in reviews: 
+        totalRating += r.rating
+    if len(reviews) > 0:
+        average_rating = totalRating / len(reviews)
+    else:
+        average_rating = 0
     context = {
         'book': book,
         'reviews': reviews,
-        'review': review,
+        'average': average_rating,
     }
     return render(request, "review.html", context)
 
-def get_reviews_ajax(request, id):
+
+def get_reviews(request, id):
+    book = Book.objects.get(pk = id)
+    review = Review.objects.filter(book=book)
+    return HttpResponse(serializers.serialize('json', review))
+
+def get_user_review(request, id):
     book = Book.objects.get(pk = id)
     review = Review.objects.filter(user=request.user, book=book)
     return HttpResponse(serializers.serialize('json', review))
 
+def edit_review(request, id, reviewId):
+    review = Review.objects.get(pk = reviewId)
+    form = ReviewForm(request.POST or None, instance=review)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return HttpResponseRedirect(reverse("reviewbook:show_review"))
+    
+    context = {'form': form}
+    return render(request, "review.html", context)
+
 @csrf_exempt
-def add_review_ajax(request):
+def add_review(request, id):
     if request.method == 'POST':
         rating = request.POST.get("rating")
         review = request.POST.get("review")
         user = request.user
+        book = Book.objects.get(pk = id)
 
-        new_review = Review(rating=rating, review=review, user=user)
+        new_review = Review(rating=rating, review=review, user=user, book=book)
         new_review.save()
 
         return HttpResponse(b"CREATED", 201)
     return HttpResponseNotFound()
 
 @csrf_exempt
-def delete_review_ajax(request, id):
+def delete_review(request, id, reviewId):
     if request.method == 'DELETE':
-        review = Review.objects.get(pk = id)
+        review = Review.objects.get(pk = reviewId)
         review.delete()
         return HttpResponse(b"DELETED", status=200)
     return HttpResponseNotFound()
