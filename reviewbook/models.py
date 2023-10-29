f9f9d8fba41caf3c78251834e24f7a6a7709e7e8
steven-fo <stevenfo0711@gmail.com>
2023-10-29 18:48:26 +0700

    feat: add username to models

--- reviewbook/models.py
+++ reviewbook/models.py
@@ -4,6 +4,7 @@ from book.models import Book
 
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     date_added = models.DateField(auto_now_add=True)
     rating = models.IntegerField()
